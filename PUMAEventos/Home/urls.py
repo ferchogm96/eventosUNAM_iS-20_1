94cf37ef313e06bf44e19f6cdc70a20d0f116612
ferchogm96 <fercho_gm96@ciencias.unam.mx>
2019-10-08 15:33:11 -0500

    forms y urls actualizads

---
+++ PUMAEventos/Home/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.urls import include, path
+from . import views
+
+app_name = 'Home'
+urlpatterns = [
+    # Funtion view
+    # path('', views.index, name='index'),
+    # Class-based Views
+    path('', views.Index.as_view(), name='index'),
+    path('about/', views.About.as_view(), name='about'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('registrarse/', views.Registrarse.as_view(), name='registrarse'),
+]
--- PUMAEventos/Home/views.py
+++ PUMAEventos/Home/views.py
@@ -1,3 +1,83 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
+
+from .utils import IsNotAuthenticatedMixin
+from Post.models import Post
+from .forms import LoginForm
+
 from django.shortcuts import render
 
-# Create your views here.
+# Class-based Views
+class Index(View):
+    """
+        Index in my Web Page but with Clased based views.
+    """
+    template = 'Home/index.html'
+    context = {'title': 'Index'}
+
+    def get(self, request):
+        """
+            Get in my Index.
+        """
+        all_posts = Post.objects.all()
+        self.context['posts'] = all_posts
+        return render(request, self.template, self.context)
+
+
+class About(View):
+    """
+        About me page.
+    """
+    template = 'Home/about.html'
+    context = {'title': 'About me'}
+
+    def get(self, request):
+        """
+            Get in About me.
+        """
+        return render(request, self.template, self.context)
+
+
+class Login(IsNotAuthenticatedMixin, View):
+    """
+        Admin login
+    """
+    template = 'Home/login.html'
+    context = {'title': 'Admin Login'}
+
+    def get(self, request):
+        """
+            Shows the form to login
+        """
+        form = LoginForm()
+        self.context['form'] = form
+
+        return render(request, self.template, self.context)
+
+    def post(self, request):
+        """
+            Validates and do the login
+        """
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                if request.GET.get("next", None) is not None:
+                    return redirect(request.GET.get("next"))
+                return redirect('Home:index')
+
+        self.context['form'] = form
+        return render(request, self.template, self.context)
+
+
+class Logout(LoginRequiredMixin, View):
+    """
+        Does the logout
+    """
+    def get(self, request):
+        logout(request)
+        return redirect("Home:index")
